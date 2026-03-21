bca3a42c5d1eae6af2772da715a70e4862d850c4
ilfat aliev <ialiev685@gmail.com>
2026-03-22 00:23:46 +0300

    рефактор редиректа

--- src/app/providers/auth/index.tsx
+++ src/app/providers/auth/index.tsx
@@ -1,31 +1,18 @@
-import { useEffect, useMemo, type PropsWithChildren } from "react";
+import { useMemo, type PropsWithChildren } from "react";
 import { AuthContext, type AuthContextType } from "./context";
 import { requestApi } from "@/services/client";
 import { useQuery } from "@tanstack/react-query";
-import { isErrorResponse } from "@/shared/lib";
-import { useNavigate } from "react-router-dom";
-import { routes } from "@/shared/configs";
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const {
     data: user,
     isLoading,
-    error,
     isError,
   } = useQuery({
     queryKey: ["user"],
     queryFn: () => requestApi.getUser(),
     staleTime: 0,
   });
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isError) {
-      if (isErrorResponse(error) && error.status === 401) {
-        navigate(routes.login);
-      }
-    }
-  }, [error, isError, navigate]);
 
   const providerValue = useMemo<AuthContextType>(
     () => ({
