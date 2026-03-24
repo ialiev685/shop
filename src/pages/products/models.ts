1a319a5de32c298bb043ff36ef53e7bd4867889c
ilfat aliev <ialiev685@gmail.com>
2026-03-24 11:05:25 +0300

    интегрировал пагинацию с апи

--- src/pages/products/models.ts
+++ src/pages/products/models.ts
@@ -1,10 +1,16 @@
 import { requestApi } from "@/services/client";
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 
 export const useController = () => {
+  const [searchParams] = useSearchParams();
+
+  const skip = Number(searchParams.get("skip") ?? 0);
+  const limit = Number(searchParams.get("limit") ?? 10);
+
   const signInMutation = useQuery({
-    queryKey: ["products"],
-    queryFn: () => requestApi.getAllProducts({ limit: 10 }),
+    queryKey: ["products", skip, limit],
+    queryFn: () => requestApi.getAllProducts({ limit, skip }),
   });
 
   return { data: signInMutation.data };
