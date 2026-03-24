91f8314d00517c393733243756f63231f63c3e6b
Ilfat Aliev <ialiev685@gmail.com>
2026-03-24 16:59:17 +0300

    правки типа Error

--- src/shared/lib.ts
+++ src/shared/lib.ts
@@ -1,4 +1,10 @@
-import type { ErrorResponse } from "@/services/Api";
+interface ErrorResponse {
+  data: null;
+  error: {
+    message: string;
+  };
+  status: number;
+}
 
 export const isErrorResponse = (error: unknown): error is ErrorResponse => {
   return typeof error === "object" && error !== null && "error" in error;
