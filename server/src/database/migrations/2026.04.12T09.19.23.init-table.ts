f492445576e7e7b7de8811bb46bab70a0c9555b4
Ilfat Aliev <ialiev685@gmail.com>
2026-04-13 11:21:04 +0300

    применил миграции

---
+++ server/src/database/migrations/2026.04.12T09.19.23.init-table.ts
@@ -0,0 +1,139 @@
+import { DataTypes } from 'sequelize';
+import { type Migration } from '../migrator';
+
+export const up: Migration = async ({ context }) => {
+  await context.createTable('type', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  await context.createTable('product', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    typeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'type_id',
+      references: {
+        model: 'type',
+        key: 'id',
+      },
+      onDelete: 'RESTRICT',
+      onUpdate: 'CASCADE',
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  await context.createTable('product_info', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'product_id',
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+
+  await context.createTable('basket', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      field: 'user_id',
+    },
+    sessionId: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      field: 'session_id',
+    },
+  });
+
+  await context.createTable('basket_products', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    basketId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'basket_id',
+      references: {
+        model: 'basket',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'product_id',
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  });
+};
+export const down: Migration = async ({ context }) => {
+  await context.dropTable('basket_products');
+  await context.dropTable('basket');
+  await context.dropTable('product_info');
+  await context.dropTable('product');
+  await context.dropTable('type');
+};
