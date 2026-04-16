e025ba70466a19a3e6b19bfa2d901c6d10d295dd
Ilfat Aliev <ialiev685@gmail.com>
2026-04-16 13:38:29 +0300

    настроил обработку ошибок по валидации

--- server/src/middleware/error-middleware.ts
+++ server/src/middleware/error-middleware.ts
@@ -1,10 +1,38 @@
 import { ApiError } from '../exception/api-errors';
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
+interface ValidationError {
+  code: 'FST_ERR_VALIDATION';
+  validation: {
+    instancePath: string;
+    message: string;
+  }[];
+}
+
+function isValidationError(error: unknown): error is ValidationError {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'code' in error &&
+    error.code === 'FST_ERR_VALIDATION' &&
+    'validation' in error
+  );
+}
+
 export const errorMiddleware = (err: unknown, req: FastifyRequest, res: FastifyReply) => {
   if (err instanceof ApiError) {
     return res.status(err.code).send({ message: err.message, errors: err.errors });
   }
+
+  if (isValidationError(err)) {
+    return res.status(400).send({
+      message: 'Ошибка валидации данных',
+      errors: err.validation
+        .map((err) => err.instancePath.replace('/', '') + ' ' + err.message)
+        .join('\n'),
+    });
+  }
+
   req.server.log.error(err);
   return res.status(500).send({ message: 'Непредвиденная ошибка', errors: err });
 };
