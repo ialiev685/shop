0987e73a458e7085e54ec40e14f96994003543d2
Ilfat Aliev <ialiev685@gmail.com>
2026-04-27 12:32:19 +0300

    сверстана карточка превью товара

---
+++ web/src/entites/preview-card/index.tsx
@@ -0,0 +1,64 @@
+import { Card, Image, Button, Text, Rating, Flex, Grid } from "@mantine/core";
+import { IconBasket } from "@tabler/icons-react";
+
+interface PreviewCardProps {
+  name: string;
+  price: number;
+  rating: number;
+  img: string;
+  onAddToBasket: () => void;
+}
+
+const PreviewCard = ({
+  name,
+  price,
+  rating,
+  img,
+  onAddToBasket,
+}: PreviewCardProps) => {
+  return (
+    <Card bg="transparent">
+      <Flex direction="column" gap={12}>
+        <Card.Section>
+          <Rating value={rating} c="accent-shop-1" readOnly />
+        </Card.Section>
+        <Card.Section>
+          <Image src={img} alt={name} />
+        </Card.Section>
+        <Card.Section>
+          <Text fz={24} fw={700} ta="right">
+            {price} руб.
+          </Text>
+        </Card.Section>
+        <Card.Section>
+          <Grid gutter={0}>
+            <Grid.Col span={3} pr={8}>
+              <Text c="gray-shop-1" fz={12}>
+                Описание
+              </Text>
+            </Grid.Col>
+            <Grid.Col span={9}>
+              <Text c="gray-shop-1" fz={12}>
+                «Спелое яблоко. Хрустит. Сочное. Насыщенный яблочный вкус с
+                кисло-сладкими нотами».
+              </Text>
+            </Grid.Col>
+          </Grid>
+        </Card.Section>
+
+        <Card.Section>
+          <Button
+            leftSection={<IconBasket cursor="pointer" />}
+            onClick={onAddToBasket}
+            w="100%"
+            variant="filled-accent-shop"
+          >
+            В корзину
+          </Button>
+        </Card.Section>
+      </Flex>
+    </Card>
+  );
+};
+
+export default PreviewCard;
