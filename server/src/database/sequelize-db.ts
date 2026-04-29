0871e0e47b640dea20628aa368f494ed33380340
ilfat aliev <ialiev685@gmail.com>
2026-04-29 13:45:03 +0300

    обновлены настройки docker

--- server/src/database/sequelize-db.ts
+++ server/src/database/sequelize-db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const isDockerMode = process.env.RUNNING_IN_DOCKER === 'true';
 
 export const sequelize = new Sequelize({
-  host: isDockerMode ? 'db' : 'localhost',
+  host: isDockerMode ? 'shop-db' : 'localhost',
   username: process.env.DB_USERNAME ?? '',
   password: process.env.DB_PASSWORD ?? '',
   port: 5432,
