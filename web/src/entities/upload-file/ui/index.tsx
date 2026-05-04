0bb7904137f640f6e0e9057c594d343f2329d0e9
ilfat aliev <ialiev685@gmail.com>
2026-05-04 22:16:26 +0300

    перенесена на проект dropzone

---
+++ web/src/entities/upload-file/ui/index.tsx
@@ -0,0 +1,57 @@
+import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
+import { Text, Flex } from "@mantine/core";
+import { IconUpload } from "@tabler/icons-react";
+import { useMantineTheme } from "@mantine/core";
+import { FileCard } from "./file-card";
+
+const fileExpansion = {
+  p7s: "application/pkcs7-signature",
+  sig: ".sig",
+  pdf: MIME_TYPES.pdf,
+  jpeg: MIME_TYPES.jpeg,
+  png: MIME_TYPES.png,
+  doc: MIME_TYPES.doc,
+  xls: MIME_TYPES.xls,
+  xlsx: MIME_TYPES.xlsx,
+} as const;
+
+type UploadFileProps = {
+  title?: string;
+  accept?: (keyof typeof fileExpansion)[];
+  onChange: (files?: File[]) => void;
+  files?: File[];
+};
+
+export const UploadFile = ({
+  title,
+  accept,
+  onChange,
+  files,
+}: UploadFileProps) => {
+  const { colors, shadows } = useMantineTheme();
+
+  return files?.length ? (
+    files.map((file) => (
+      <FileCard title={file.name} onClose={() => onChange(undefined)} />
+    ))
+  ) : (
+    <Dropzone
+      multiple={false}
+      accept={accept?.map((expansion) => fileExpansion[expansion])}
+      onDrop={onChange}
+      style={{ boxShadow: shadows.xs }}
+    >
+      <Flex gap={12} align="center" justify="center">
+        <IconUpload color={colors.gray[8]} />
+        <div>
+          <Text c={colors.gray[8]}>{title}</Text>
+          {accept && (
+            <Text size="sm" c={colors.gray[6]}>
+              {accept?.map((expansion) => expansion).join(", ")}
+            </Text>
+          )}
+        </div>
+      </Flex>
+    </Dropzone>
+  );
+};
