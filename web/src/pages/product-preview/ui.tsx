b54d0c86515d086d01f38e0065e141cf41b6d8f8
ilfat aliev <ialiev685@gmail.com>
2026-05-15 16:18:08 +0300

    добавлен динамическое  название просмотра товара

--- web/src/pages/product-preview/ui.tsx
+++ web/src/pages/product-preview/ui.tsx
@@ -1,40 +1,27 @@
 import { PreviewCard } from "@/entities/product/ui/preview-card";
-import { productQueries } from "@/entities/product";
 import { AddProductToBasketControl } from "@/features/add-product--to-basket-control/ui";
 import { Flex, Title } from "@mantine/core";
 import { IconBasket } from "@tabler/icons-react";
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import { productInfoQueries } from "@/entities/product-info/api/product-info-queries";
+import { useController } from "./model";
 
 export const ProductPreview = () => {
-  const { id } = useParams<string>();
+  const { product, productInfo, title } = useController();
 
-  const productQuery = useQuery({
-    ...productQueries.getById(Number(id)),
-    enabled: Boolean(id),
-  });
-
-  const productInfoQuery = useQuery({
-    ...productInfoQueries.getById(productQuery.data?.id ?? NaN),
-    enabled: Boolean(productQuery.data?.id),
-  });
-
-  if (!productQuery.data) {
+  if (!product) {
     return null;
   }
 
   return (
     <Flex direction="column" gap={24}>
       <Title order={2} c="gray-shop-1">
-        Предпросмотр товара
+        {title}
       </Title>
       <PreviewCard
-        productInfo={productInfoQuery.data}
-        {...productQuery.data}
+        productInfo={productInfo}
+        {...product}
         control={
           <AddProductToBasketControl
-            productId={productQuery.data.id}
+            productId={product.id}
             leftSection={<IconBasket cursor="pointer" />}
             variant="filled-accent-shop"
           />
